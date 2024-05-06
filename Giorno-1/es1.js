75915260fc9bb2df8dcf905e0ff021ad18eca878
Manuel Pinheiro <manuel.pinheiro@libero.it>
2024-05-06 16:30:04 +0200

    first commit

---
+++ Giorno-1/es1.js
@@ -0,0 +1,24 @@
+class Utente {
+  constructor(_firstName, _lastName, _age, _location) {
+    this.firstName = _firstName;
+    this.lastName = _lastName;
+    this.age = _age;
+    this.location = _location;
+  }
+
+  confrontaEta(altroUtente) {
+    if (this.age > altroUtente.age) {
+      return `${this.firstName} è più vecchio di ${altroUtente.firstName}.`;
+    } else if (this.age < altroUtente.age) {
+      return `${altroUtente.firstName} è più vecchio di ${this.firstName}.`;
+    } else {
+      return `${this.firstName} e ${altroUtente.firstName} hanno la stessa età.`;
+    }
+  }
+}
+
+// Esempio di utilizzo:
+const utente1 = new Utente("Mario", "Rossi", 30, "Roma");
+const utente2 = new Utente("Luigi", "Verdi", 25, "Milano");
+
+console.log(utente1.confrontaEta(utente2)); // Stampa: "Mario è più vecchio di Luigi."
---
+++ Giorno-1/script.js
@@ -0,0 +1,72 @@
+const inputName = document.getElementById("petName");
+const inputOwnerName = document.getElementById("ownerName");
+const inputSpecies = document.getElementById("species");
+const inputBreed = document.getElementById("breed");
+const formTag = document.getElementsByTagName("form")[0];
+
+const contacts = []; // Array per memorizzare i contatti
+
+class Pets {
+  constructor(_petName, _ownerName, _species, _breed, _isSameOwner) {
+    this.petName = _petName;
+    this.ownerName = _ownerName;
+    this.species = _species;
+    this.breed = _breed;
+    this.isSameOwner = _isSameOwner; // Proprietà che indica se il padrone è lo stesso del contatto precedente
+  }
+}
+
+const updateContacts = function () {
+  const contactsRow = document.getElementById("contacts-row");
+
+  contactsRow.innerHTML = "";
+
+  contacts.forEach((pet) => {
+    const newDiv = document.createElement("div");
+
+    newDiv.classList.add("col");
+
+    newDiv.innerHTML = `
+      <div class="card${
+        pet.isSameOwner ? " border border-success border-3" : ""
+      }">
+        <div class="card-body">
+          <h5 class="card-title">${pet.petName}</h5>
+          <h5 class="card-title">${pet.ownerName}</h5>
+          <h5 class="card-title">${pet.species}</h5>
+          <h5 class="card-title">${pet.breed}</h5>
+        </div>
+      </div>
+    `;
+
+    contactsRow.appendChild(newDiv);
+  });
+};
+
+formTag.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Verifica se il padrone è lo stesso del contatto precedente
+  const sameOwner =
+    contacts.length > 0 &&
+    inputOwnerName.value === contacts[contacts.length - 1].ownerName;
+
+  const contactFromFormValues = new Pets(
+    inputName.value,
+    inputOwnerName.value,
+    inputSpecies.value,
+    inputBreed.value,
+    sameOwner // Passa true se il padrone è lo stesso del contatto precedente, altrimenti false
+  );
+
+  contacts.push(contactFromFormValues);
+
+  console.log("CONTATTO CREATO", contactFromFormValues);
+
+  inputName.value = "";
+  inputOwnerName.value = "";
+  inputSpecies.value = "";
+  inputBreed.value = "";
+
+  updateContacts();
+});
